5e10ab6a45d71ad5a94eafe9015efe59710dfad3
unknown <austin8897@gmail.com>
2018-11-24 21:45:40 -0600

    initial commit --- gitignore broken

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/index.html', function (req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/about.html', function (req, res) {
+  res.sendFile(path.join(__dirname+'/about.html'));
+});
+
+app.listen(3000);
